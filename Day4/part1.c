74ae73c34f733aad19189a8eee2c603f1dcd8a37
Chantal Ariu <chantalariu227@gmail.com>
2023-12-27 21:50:49 +0100

    Day 4 part 1 solutions

---
+++ Day4/part1.c
@@ -0,0 +1,206 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+char *strip(char *str)
+{
+    bool isEmpty = strlen(str) == 0;
+    bool isSingularAndWhitespace = strlen(str) == 1 && (str[0] == ' ' || str[0] == '\n');
+
+    if (isEmpty || isSingularAndWhitespace)
+    {
+        char *stripped_str = malloc(sizeof(char));
+        stripped_str[0] = '\0';
+        return stripped_str;
+    }
+
+    int l;
+    int r;
+
+    for (int i = 0; i < strlen(str); i++)
+    {
+        l = i;
+
+        if (str[i] != ' ' && str[i] != '\n')
+        {
+            break;
+        }
+    }
+
+    for (int i = strlen(str) - 1; i >= 0; i--)
+    {
+        r = i + 1;
+
+        if (str[i] != ' ' && str[i] != '\n')
+        {
+            break;
+        }
+    }
+
+    if (l > r)
+    {
+        char *stripped_str = malloc(sizeof(char));
+        stripped_str[0] = '\0';
+        return stripped_str;
+    }
+
+    char *stripped_str = malloc(sizeof(char) * (r - l + 1));
+    memcpy(stripped_str, str + l, r - l + 1);
+    stripped_str[r - l] = '\0';
+
+    return stripped_str;
+}
+
+char **split(char *str, char *seperators, int *count)
+{
+    int len = strlen(str);
+    *count = 0;
+
+    int i = 0;
+    while (i < len)
+    {
+        while (i < len)
+        {
+            if (strchr(seperators, str[i]) == NULL)
+                break;
+            i++;
+        }
+
+        int prev_i = i;
+        while (i < len)
+        {
+            if (strchr(seperators, str[i]) != NULL)
+                break;
+            i++;
+        }
+
+        if (i > prev_i)
+            *count = *count + 1;
+    }
+
+    char **strings = malloc(sizeof(char *) * *count);
+
+    i = 0;
+    char buffer[16384];
+    int str_ind = 0;
+
+    while (i < len)
+    {
+        while (i < len)
+        {
+            if (strchr(seperators, str[i]) == NULL)
+                break;
+            i++;
+        }
+
+        int j = 0;
+        while (i < len)
+        {
+            if (strchr(seperators, str[i]) != NULL)
+                break;
+
+            buffer[j] = str[i];
+            i++;
+            j++;
+        }
+        if (j > 0)
+        {
+            buffer[j] = '\0';
+
+            int to_allocate = sizeof(char) * (strlen(buffer) + 1);
+
+            strings[str_ind] = malloc(to_allocate);
+
+            strcpy(strings[str_ind], buffer);
+
+            str_ind++;
+        }
+    }
+    return strings;
+}
+
+int main()
+{
+    FILE *fptr = fopen("input.txt", "r");
+
+    long flength = 0;
+
+    fseek(fptr, 0, SEEK_END);
+    flength = ftell(fptr);
+    fseek(fptr, 0, SEEK_SET);
+
+    char *fcontent = malloc(sizeof(char) * (flength + 1));
+
+    for (long i = 0; i < flength; i++)
+    {
+        fcontent[i] = getc(fptr);
+    }
+
+    fcontent[flength] = '\0';
+
+    char first_part[25000];
+
+    char second_part[25000];
+
+    int lines_count = 0;
+
+    char **lines = split(fcontent, "\n", &lines_count);
+
+    int final_sum = 0;
+
+    for (int i = 0; i < lines_count; i++)
+    {
+        char *strip_lines = strip(lines[i]);
+        int game_count = 0;
+        char **cur_game = split(strip_lines, ":", &game_count);
+        char *game_id = cur_game[0];
+        char *game_cards = cur_game[1];
+        char *strip_game_id = strip(game_id);
+        char *strip_game_cards = strip(game_cards);
+
+        int card_set_count = 0;
+        char **card_sets = split(strip_game_cards, "|", &card_set_count);
+        char *lucky_numbers = card_sets[0];
+        char *my_numbers = card_sets[1];
+
+        char *lucky_numbers_stripped = strip(lucky_numbers);
+        char *my_numbers_stripped = strip(my_numbers);
+
+        int lucky_count = 0;
+        char **lucky_numbers_split = split(lucky_numbers_stripped, " ", &lucky_count);
+
+        int my_count = 0;
+        char **my_numbers_split = split(my_numbers_stripped, " ", &my_count);
+
+        int counter = 0;
+
+        for (int j = 0; j < lucky_count; j++)
+        {
+            int j_int = atoi(lucky_numbers_split[j]);
+
+            for (int k = 0; k < my_count; k++)
+            {
+                int k_int = atoi(my_numbers_split[k]);
+
+                if (j_int == k_int)
+                {
+                    counter++;
+                }
+            }
+        }
+
+        if (counter == 0)
+        {
+            continue;
+        }
+
+        int card_value = pow(2, counter - 1);
+        final_sum += card_value;
+        printf("%d ^ %d = %d\n", 2, counter - 1, card_value);
+    }
+    printf("final sum: %d", final_sum);
+
+    return 0;
+}
